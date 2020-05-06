e56015449108883bd99cb9074e30a93028400820
Muhammad Binyameen <Muhammad.Binyameen@futurenow.com>
2020-05-06 20:38:55 +0500

    Adding fils

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+    	return view('admin\users',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return back()->with(['success' => 'User role is deleted successfully.']);
+    }
+
+    public function change_role(User $user)
+    {
+        if( $user->user_role == 1 )
+        $user->user_role = 2;
+        elseif( $user->user_role == 2 )
+        $user->user_role = 1;
+
+        $user->save();
+
+        return back()->with(['success' => 'User role is changed successfully.']);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Auth::routes();
+
+Route::get('/home', 'PagesController@home')->name('home');
+Route::get('about','PagesController@about');
+Route::get('contact_us','PagesController@contact_us');
+Route::get('hotel','PagesController@hotels');
+Route::get('hotel_events','PagesController@events');
+
+
+Route::get('family_rooms','RoomController@family_rooms');
+Route::get('premium_rooms','RoomController@premium_rooms');
+
+Route::resource('users','UserController');
+Route::resource('event_categories','EventCategoriesController');
+Route::resource('events','EventsController');
+Route::resource('facilities','FacilitiesController');
+Route::resource('hotels','HotelsController');
+Route::resource('reviews','ReviewsController');
+Route::view('dashboard','admin/index')->middleware('auth');
+Route::get('change_user_role/{user}','UserController@change_role');
+Route::post('search_hotel','HotelsController@search_hotel');
+
+Route::get('test',function(){
+   $rating = \App\Reviews::first();
+   $hotel = \App\Hotels::first();
+   $events = \App\Events::latest()->get();
+   	
+   	foreach( $events as $event)
+   	{
+   		echo "<p>".$event->images->first()['path'].$event->images->first()['unique_identifier']."</p>";
+   	}
+   // return $hotel->events->first();
+
+});
